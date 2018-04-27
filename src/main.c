b9c539f37670e75220140ea3aa9b2da84981fbc7
eiire <bulax.d.2101@gmail.com>
2018-04-27 21:02:01 +0700

    "Алгоритм качелей"

--- src/main.c
+++ src/main.c
@@ -5,7 +5,27 @@ int main()
 	printf("Приветствую в игре 100 спичек, я робот, который будет против тебя играть! Выбери каким ты хочешь ходить: ");
 	int turn = check_turn();
 
-	int input, stroke_check, matches_remain = 100;
+	int input, stroke_check, matches_remain = 100, remainder;
+
+	while (matches_remain < 100 && matches_remain != 2 && matches_remain != 0) {
+		if (turn == 2) {
+			turn = check_turn_inversion(turn);
+			// Для компа
+		} else {
+			turn = check_turn_inversion(turn);
+			// Для чела
+		}
+	}
+
+	if(turn == 2) {
+		printf("Вы проиграли!");
+	} else {
+		printf("Вы выиграл!");
+	}
 
 	return 0;
 }
+/*
+В правильности работы не уверен, нужно дописать код позже.
+[НАПОМИНАНИЕ] Работает по принципу качелей. Похожий трюк реализовывал, когда писал шахматы(Почему качелей, наверное, пойму только я, надо будет, сприсишь))
+*/
