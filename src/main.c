23ed70e4acefcf9de343c04478b4d6ba045ac823
eiire <bulax.d.2101@gmail.com>
2018-04-28 00:54:44 +0700

    Доработка основного алгоритма. Проект находится на стадии BETA

--- src/main.c
+++ src/main.c
@@ -1,26 +1,32 @@
 #include "matches.h"
 
+#include "matches.h"
+
 int main()
 {
 	printf("Приветствую в игре 100 спичек, я робот, который будет против тебя играть! Выбери каким ты хочешь ходить: ");
 	int turn = check_turn();
 
-	int input, stroke_check, matches_remain = 100, remainder;
+	int input, stroke_check, matches_remain = 100, buffer;
 
-	while (matches_remain < 100 && matches_remain != 2 && matches_remain != 0) {
+	while (matches_remain < 101 && matches_remain != 2 && matches_remain != 0) {
 		if (turn == 2) {
 			turn = check_turn_inversion(turn);
-			// Для компа
+			buffer = rand() % 10;
+			matches_remain =  matches_remain - buffer;
+			printf("Я взял %d сталось %d. Твой ход!\n", buffer, matches_remain);
 		} else {
 			turn = check_turn_inversion(turn);
-			// Для чела
+			scanf("%d", &buffer);
+			matches_remain =  matches_remain - buffer;
+			printf("Осталось %d\n", matches_remain);
 		}
 	}
 
 	if(turn == 2) {
-		printf("Вы проиграли!");
+		printf("Вы проиграли!\n");
 	} else {
-		printf("Вы выиграл!");
+		printf("Вы выиграли!\n");
 	}
 
 	return 0;
