fcd4a48fb5c9ceaeab82e7bbeb84b219bc7b4585
VitekUn <U10022000@yandex.ru>
2018-05-09 16:39:35 +0700

    Доработка вывода

--- src/main.c
+++ src/main.c
@@ -1,14 +1,10 @@
 #include "matches.h"
+#include "name.h"
 
 int main()
 {
 
-	int row, col;
-	const char *hello = "Welcome to the game 100 matches!";
-	const char *robot = "I am a robot that will be against you";
-	const char *choice = "Choose how you want to go: ";
-	const char *won = "You won!";
-	const char *lose = "You lose!";
+	int row, col, stroka = 0;
 
 	initscr();
 
@@ -17,13 +13,13 @@ int main()
 	curs_set(0);
 	echo();
 
-	mvwprintw(stdscr, 0 , (col - strlen(hello)) / 2, "%s", hello);
-	mvwprintw(stdscr, 1 , (col - strlen(robot)) / 2, "%s", robot);
-	mvwprintw(stdscr, 2 , 0 , "%s", choice);
+	mvwprintw(stdscr, stroka = stroka + 3 , (col - strlen(hello)) / 2, "%s", hello);
+	mvwprintw(stdscr, stroka = stroka + 3 , (col - strlen(robot)) / 2, "%s", robot);
+	mvwprintw(stdscr, row / 2 , (col - strlen(choice)) / 2 , "%s", choice);
 
 	int turn = check_turn();
 	int first = turn;
-
+	clear();
 	int input, stroke_check, matches_remain = 100, buffer;
 
 	while (matches_remain >= 0 && matches_remain < 101
@@ -32,14 +28,23 @@ int main()
 			turn = turn_inversion(turn);
 			strategy(matches_remain, first, &buffer);
 			matches_remain = matches_remain - buffer;
-			printw("I took %d left %d. Your move!\n", buffer, matches_remain);
+			if (matches_remain >= 96)			
+				stroka = -1;
+			mvwprintw(stdscr, stroka = stroka + 1 , (col - strlen(left)) / 2 - 30, "%s", took);
+			mvwprintw(stdscr, stroka , (col - strlen(left)) / 2 - 22, "%d", buffer);
+			mvwprintw(stdscr, stroka , (col - strlen(left)) / 2 , "%s", left);
+			mvwprintw(stdscr, stroka , ((col - strlen(left)) / 2 + strlen(left) + 2) , "%d", matches_remain);
+			mvwprintw(stdscr, stroka , ((col - strlen(ymove)) / 2) + 30 , "%s", ymove);
 		} else {
 			turn = turn_inversion(turn);
+			if (matches_remain == 100)
+				mvwprintw(stdscr, stroka = 0 , (col - strlen(nunb)) / 2 , "%s", nunb);
 			scanw("%d", &input);
 			stroke_check = check_input(input);
 			if (stroke_check == 1) {
 				matches_remain = matches_remain - input;
-				printw("Left %d\n", matches_remain);
+				mvwprintw(stdscr, stroka = stroka + 1 , (col - strlen(left)) / 2, "%s", left);
+				mvwprintw(stdscr, stroka , (col - strlen(left)) / 2 + strlen(left) + 1, "%d", matches_remain);
 			} else {
 				turn = turn_inversion(turn);
 				printw("You entered an invalid value, please try again: ");
