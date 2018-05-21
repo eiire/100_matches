3ce2aa885aa42fd371fcde14331e8250c709c1c9
eiire <bulax.d.2101@gmail.com>
2018-05-22 00:20:43 +0700

    Тест + исправил серьезную ошибку в стратегии компа, когда человек делает ошибку в предпоследнем ходе и при определенном колличестве спичек

--- test/test.c
+++ test/test.c
@@ -20,3 +20,20 @@ CTEST(numOfRemain, check_turn)
 	ASSERT_EQUAL(expected2, check2);
 
 }
+
+CTEST(numOfRemain, check_result)
+{
+	// Проверка на выигрыш в двух случаях
+	int expected_win = 1;
+	int check_win = check_result(1, -1, 0, 1);
+	ASSERT_EQUAL(expected_win, check_win);
+
+	// Проверка на проигрыш в двух случаях
+	int expected_lose = 0;
+	int check_lose = check_result(2, -1, 0, 1);
+	ASSERT_EQUAL(expected_lose, check_lose);
+
+	int expected = 3;
+	int check = check_result(0, 0, 0, 0);
+	ASSERT_EQUAL(expected, check);
+}
