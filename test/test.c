3c2505d04c46beace27d713c16611bf4ef2ae935
eiire <bulax.d.2101@gmail.com>
2018-05-22 16:06:04 +0700

    Корректировка тестов

--- test/test.c
+++ test/test.c
@@ -1,39 +1,46 @@
 #include <ctest.h>
 #include <matches.h>
 
-CTEST(numOfRemain, check_turn)
+CTEST(check_queue, check_turn)
 {
 	// GIVEN
 	int check = turn_inversion(1);
-
-	// THEN
+	// WHEN
 	int expected = 2;
-
+	// THEN
 	ASSERT_EQUAL(expected, check);
 
 	// GIVEN
 	int check2 = turn_inversion(2);
-
-	// THEN
+	// WHEN
 	int expected2 = 1;
-
+	// THEN
 	ASSERT_EQUAL(expected2, check2);
-
 }
 
-CTEST(numOfRemain, check_result)
+CTEST(check_result, check_win_lose_end)
 {
-	// Проверка на выигрыш в двух случаях
+	// GIVEN
 	int expected_win = 1;
+	// WHEN
 	int check_win = check_result(1, -1, 0, 1);
+	// THEN
 	ASSERT_EQUAL(expected_win, check_win);
 
-	// Проверка на проигрыш в двух случаях
+	// GIVEN
 	int expected_lose = 0;
+	// WHEN
 	int check_lose = check_result(2, -1, 0, 1);
+	// THEN
 	ASSERT_EQUAL(expected_lose, check_lose);
 
+	// GIVEN
 	int expected = 3;
+	// WHEN
 	int check = check_result(0, 0, 0, 0);
+	// THEN
 	ASSERT_EQUAL(expected, check);
 }
+
+
+
